45aedc6bdc5523cf59b525588440ff39828350d6
Nate Stuyvesant <nstuyvesant@gmail.com>
2020-05-24 14:07:44 -0400

    cleanup(App): eslint warnings

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,6 @@ import { Component, Vue } from "vue-property-decorator";
 import Navbar from "./components/Navbar.vue";
 import Designer from "./components/Designer.vue";
 
-// Nice article on state management
-// https://vueschool.io/articles/vuejs-tutorials/deep-dive-into-vue-state-management/
-// Perhaps manage the form here and pass as props to Navbar and Designer and have them emit
-// events so we can dumb this thing down?
 @Component({
   components: {
     Navbar,
@@ -21,3 +17,14 @@ import Designer from "./components/Designer.vue";
 })
 export default class App extends Vue {}
 </script>
+
+<style lang="scss">
+body,
+td,
+th,
+input,
+textarea,
+select {
+  -webkit-font-smoothing: antialiased;
+}
+</style>
