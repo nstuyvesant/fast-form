99be45c7f60b2c59ce60fda74aa4505e896b7b02
Nate Stuyvesant <nstuyvesant@gmail.com>
2020-05-26 10:15:05 -0400

    test(e2e): check visibility

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -43,7 +43,29 @@ describe("Initial test", () => {
 
   it("Adds section and error disappears", () => {
     cy.get(".add-section").click();
-    cy.contains("#sectionFeedback").should("have.css","display", "none");
+    cy.get("#sectionFeedback").should("have.css", "display", "none");
   });
+
+  // Add questions, they appear in list, textarea is reset to empty, and error disappears
+
+  // Clear question label and look for error
+
+  // Set question label and look for error to clear
+
+  // Delete question and it's gone
+
+  // Add another question and it's added
+
+  // Change dataType of a question
+
+  // Change required checkbox of a question
+
+  // Click Preview and verify alert is displayed
+
+  // Dismiss alert
+
+  // Download JSON of form
+
+  // Refresh webpage then Upload JSON and verify it loaded
 
 });
