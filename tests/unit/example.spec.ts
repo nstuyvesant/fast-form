cedd53f96df29d462ffb99b2416824b153dd7090
Nate Stuyvesant <nstuyvesant@gmail.com>
2020-06-01 23:02:32 -0400

    chore(app): bump dependencies

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,8 +1,18 @@
 import { shallowMount } from "@vue/test-utils";
 import Designer from "@/components/Designer.vue";
 
+const wrapper = shallowMount(Designer);
+
 describe("Designer.vue", () => {
-  it("renders form with  name and one section by default", () => {
+  it("has a name property", () => {
+    expect(typeof Designer.name).toBe("function");
+  });
+
+  it("has a name property", () => {
+    // expect(typeof Designer.).toBe('function')
+  });
+
+  it("renders form with name and one section by default", () => {
     const msg = "new message";
     const wrapper = shallowMount(Designer, {
       propsData: { msg },
